3a1ff52f5eac11406a1f304a7dc788c04db4a0ab
Ali <vynz420@gmail.com>
2025-03-07 22:05:53 +0500

    asd

--- WTranscriptor/SVClient.py
+++ WTranscriptor/SVClient.py
@@ -128,7 +128,7 @@ class ASRClient:
             return audio_path
         else:
             print('VAD Did Not Detect a Speech')
-            return None
+            return audio_data
         
 
     def transcribe_audio_array(self, audio_array, sample_rate=16000, lang="en"):
